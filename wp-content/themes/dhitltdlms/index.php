02168cf157b73725b1e289099614e9f0b2c4776b
RusulAzomSumon <18103020@iubat.edu>
2022-09-18 22:54:54 +0600

    added cs and js bootstrap

--- wp-content/themes/dhitltdlms/index.php
+++ wp-content/themes/dhitltdlms/index.php
@@ -5,10 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      href="./assets/css/bootstrap.css"
       rel="stylesheet"
-      integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
-      crossorigin="anonymous"
     />
     <link
       rel="stylesheet"
@@ -18,14 +16,14 @@
       href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css">
     <title>DHIT LMS</title>
   </head>
   <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
       <div class="container">
-        <img src="./img/dhitltdLogo.png" alt="dhitlogo" class="img-fluid" width="50px" height="auto">
+        <img src="./assets/img/dhitltdLogo.png" alt="dhitlogo" class="img-fluid" width="50px" height="auto">
         <a href="#"></a>
         <button
           class="navbar-toggler"
@@ -909,9 +907,7 @@
     </div>
 
     <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-      crossorigin="anonymous"
+      src="./assets/js/bootstrap.bundle.js"
     ></script>
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js"></script>
 
